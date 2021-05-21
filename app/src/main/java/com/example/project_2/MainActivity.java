4416af049f0600425b93af192247eece64f593dd
mduf257 <80430258+mduf257@users.noreply.github.com>
2021-05-21 17:49:05 +1200

    Start of Order App stuff

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.project_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    class ViewHolder {
+        EditText nameEditText;
+
+        public ViewHolder() {
+            nameEditText = findViewById(R.id.name_edit_text);
+        }
+    }
+
+    ViewHolder vh;
+    List<String> toppingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vh = new ViewHolder();
+        toppingsList = new ArrayList<String>();
+    }
+
+    public void toppingsCheckOnclick(View v) {
+        CheckBox chk = (CheckBox) v;
+        if (chk.isChecked())
+            toppingsList.add(chk.getText().toString());
+        else
+            toppingsList.remove(chk.getText().toString());
     }
 }
