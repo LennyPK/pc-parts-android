08d3b6dca862e54db28fab4b6940f0cae9899033
mduf257 <80430258+mduf257@users.noreply.github.com>
2021-05-25 18:21:17 +1200

    Started to make home screen of our app

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -13,30 +13,16 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     class ViewHolder {
-        EditText nameEditText;
-
         public ViewHolder() {
-            nameEditText = findViewById(R.id.name_edit_text);
+
         }
     }
 
     ViewHolder vh;
-    List<String> toppingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        vh = new ViewHolder();
-        toppingsList = new ArrayList<String>();
-    }
-
-    public void toppingsCheckOnclick(View v) {
-        CheckBox chk = (CheckBox) v;
-        if (chk.isChecked())
-            toppingsList.add(chk.getText().toString());
-        else
-            toppingsList.remove(chk.getText().toString());
     }
 }
